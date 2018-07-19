e8912a679b7d7c384641a36ee459773ed25d3bd1
Samuel Thomas <samvthom16@gmail.com>
2018-07-19 15:15:14 +0530

    Include poppers script

--- functions.php
+++ functions.php
@@ -1,22 +1,22 @@
 <?php
- 
-function custom_theme_assets() {
-	wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '1.1.0' );
 	
-	wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js', array('jquery'), '1.0', true);
-	
-	wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' );
-	
-	wp_enqueue_style('font-awesome', get_template_directory_uri() . '/font-awesome/css/font-awesome.css'); 
+	add_action( 'wp_enqueue_scripts', function(){
+		wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '1.1.0' );
+		
+		wp_deregister_script('jquery');
+		
+		wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), null, true );
+		
+		wp_enqueue_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array(), null, true );
+		
+		wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js', array('jquery', 'popper'), '1.0', true );
+		
+		/* ENQUEUE STYLES */
+		wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' );
+		wp_enqueue_style('font-awesome', get_template_directory_uri() . '/font-awesome/css/font-awesome.css'); 
+		wp_enqueue_style ('theme-style', get_template_directory_uri().'/medium.css');	
+		/* ENQUEUE STYLES */
+	} );
 
-	wp_enqueue_style ('theme-style', get_template_directory_uri().'/medium.css');	
 
-	
-}	
-	
-	
-	
-add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
-
-
-add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
