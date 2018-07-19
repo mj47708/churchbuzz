bc13f5a0469a71faaf5aace1337ae7caf481b48f
Samuel Thomas <samvthom16@gmail.com>
2018-07-19 14:32:28 +0530

    Fix bug on enqueue

--- index.php
+++ index.php
@@ -1,39 +1,29 @@
+<?php get_header();?>
 <?php
- 
-get_header();
- 
-if ( have_posts() ) :
-	while ( have_posts() ) : the_post(); ?>
-		<div class="container">
-			<article class="grid-box">
-				<div class="grid-content">
-					<?php the_post_thumbnail("medium");?>
+	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<div class="container">
+		<article class="grid-box">
+			<div class="grid-content"><?php the_post_thumbnail("medium");?></div>
+			<div class="title-content">
+				<div class="grid-author-title">
+					<div class="avatar-shape pt-2"><?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></div>
+					<div class="author-name">
+						<a href="#"><?php echo get_the_author_link(); ?></a><br>
+						<p><?php echo get_the_date(); ?></p>
+					</div>
 				</div>
-				<div class="title-content">
-					<div class="grid-author-title">
-						<div class="avatar-shape pt-2"><?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></div>
-						
-						<div class="author-name">
-							<a href="#"><?php echo get_the_author_link(); ?></a><br>
-							<p><?php echo get_the_date(); ?></p>
-						</div>
-					</div>
-					<div class="grid-item">
-						<h5><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-					
+				<div class="grid-item">
+					<h5><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
 					<?php the_content(); ?><h7><a href="<?php the_permalink()?>">continue reading..</a></h7>
-					</div>
-				</div>	
-			</article>
-		</div>
-
-	<?php endwhile;
+				</div>
+			</div>	
+		</article>
+	</div>
+<?php endwhile;
  
-else :
-	echo '<p>There are no posts!</p>';
+	else :
+		echo '<p>There are no posts!</p>';
  
-endif;
- 
-get_footer();
- 
+	endif;
 ?>
+<?php get_footer();?>
