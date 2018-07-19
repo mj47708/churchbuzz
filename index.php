35964860cdc681abd186176537814c510ffd2d8d
Samuel Thomas <samvthom16@gmail.com>
2018-07-19 14:47:35 +0530

    Create partials forlder for template parts

--- index.php
+++ index.php
@@ -1,29 +1,13 @@
 <?php get_header();?>
-<?php
-	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="container">
-		<article class="grid-box">
-			<div class="grid-content"><?php the_post_thumbnail("medium");?></div>
-			<div class="title-content">
-				<div class="grid-author-title">
-					<div class="avatar-shape pt-2"><?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></div>
-					<div class="author-name">
-						<a href="#"><?php echo get_the_author_link(); ?></a><br>
-						<p><?php echo get_the_date(); ?></p>
-					</div>
-				</div>
-				<div class="grid-item">
-					<h5><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-					<?php the_content(); ?><h7><a href="<?php the_permalink()?>">continue reading..</a></h7>
-				</div>
-			</div>	
-		</article>
+<div class="container" style="margin-top: 40px;margin-bottom: 40px;">
+	<div class='row'>
+		<div class='col-md-12'>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'partials/post', 'list' );?>
+		<?php endwhile; else :?>
+			<p>There are no posts!</p>
+		<?php endif;?>
+		</div>
 	</div>
-<?php endwhile;
- 
-	else :
-		echo '<p>There are no posts!</p>';
- 
-	endif;
-?>
+</div>
 <?php get_footer();?>
---
+++ partials/post-list.php
@@ -0,0 +1,11 @@
+<article class="grid-box">
+	<div class="grid-content"><?php the_post_thumbnail("medium");?></div>
+	<div class="title-content">
+		<div class='text-muted small' style='margin-bottom: 20px;'><a href="<?php the_author_link();?>"><?php the_author(); ?></a> on <?php echo get_the_date(); ?></div>
+		<div class="grid-item">
+			<h5><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+			<?php the_content(); ?>
+			<a href="<?php the_permalink()?>">Continue reading ...</a>
+		</div>
+	</div>	
+</article>
