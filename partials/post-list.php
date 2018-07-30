653a4aec1a1528e0065fd8b93949aeea8357d76f
Samuel Thomas <samvthom16@gmail.com>
2018-07-30 18:35:02 +0530

    Update styles for grid

--- partials/post-list.php
+++ partials/post-list.php
@@ -1,5 +1,13 @@
+<?php
+	global $post_id;
+	$url = '';
+	$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'medium' );
+	if( is_array( $thumbnail ) ){
+		$url = $thumbnail[0];
+	}
+?>
 <article class="grid-box">
-	<div class="grid-content"><?php the_post_thumbnail("medium");?></div>
+	<div class="grid-content" style="background-image: url('<?php _e( $url );?>');"><?php //the_post_thumbnail("medium");?></div>
 	<div class="title-content">
 		<div class='text-muted small' style='margin-bottom: 20px;'><a href="<?php the_author_link();?>"><?php the_author(); ?></a> on <?php echo get_the_date(); ?></div>
 		<div class="grid-item">
