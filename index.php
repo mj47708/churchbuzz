484268ff6a1eba77856d0bb4aec0a01c415e7743
Samuel Thomas <samvthom16@gmail.com>
2018-07-30 18:09:11 +0530

    Include templates for orbit-bundle

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<div class='row'>
 		<div class='col-md-12'>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
-			<?php get_template_part( 'partials/post', 'list' ); ?>
+			<?php the_content(); ?>
 		<?php endwhile; else :?>
 			<p>There are no posts!</p>
 		<?php endif;?>
---
+++ orbit-query/articles-list.php
@@ -0,0 +1,5 @@
+<div id="<?php _e($atts['id']);?>" class="col-12" data-target="<?php _e('article.grid-box');?>" data-url="<?php _e( $atts['url'] );?>">
+<?php while( $this->query->have_posts() ) : $this->query->the_post();?>
+	<?php get_template_part( 'partials/post', 'list' );?>
+<?php endwhile;?>
+</div>
